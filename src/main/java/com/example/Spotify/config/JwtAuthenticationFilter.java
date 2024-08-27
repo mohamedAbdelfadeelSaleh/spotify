547e637640524fc1870212aa3218d9fb478408c8
mohamed haroun saleh <es-mosaleh.saleh2024@alexu.edu.eg>
2024-08-28 02:55:19 +0300

    Adding JWT Authentication Filter adding Sercurity Configuration will be customized soon creating SigninRequest, signUpRequest implementing UserDetails

---
+++ src/main/java/com/example/Spotify/config/JwtAuthenticationFilter.java
@@ -0,0 +1,59 @@
+package com.example.Spotify.config;
+
+import com.example.Spotify.service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JWTService jwtService;
+    private final UserService userService;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+
+        if (StringUtils.isEmpty(authorizationHeader) || !StringUtils.startsWith(authorizationHeader, "Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = authorizationHeader.substring(7);
+        userEmail = jwtService.extractUserName(jwt);
+
+        if (StringUtils.isNotEmpty(userEmail) && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = userService.userDetailsService().loadUserByUsername(userEmail);
+
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
+                        userDetails, null, userDetails.getAuthorities()
+                );
+                token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(token);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
+
+
+
+
+
+}
---
+++ src/main/java/com/example/Spotify/dto/JwtAuthenticationResponse.java
@@ -0,0 +1,11 @@
+package com.example.Spotify.dto;
+
+
+import lombok.Data;
+
+@Data
+public class JwtAuthenticationResponse {
+    private String token;
+    private String refreshToken;
+
+}
