3812238328e41e308a1485c13675c299067061e7
mohamed haroun saleh <es-mosaleh.saleh2024@alexu.edu.eg>
2024-08-28 02:41:49 +0300

    adding spring security creating enum for Role implementing UserDetails

--- src/main/java/com/example/Spotify/model/User.java
+++ src/main/java/com/example/Spotify/model/User.java
@@ -1,13 +1,18 @@
 package com.example.Spotify.model;
 
+import com.example.Spotify.enums.Role;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -16,7 +21,7 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 @Table(name = "_user")
-public class User{
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     private Long id;
@@ -24,6 +29,7 @@ public class User{
     private String lastName;
     private String email;
     private String password;
+    private Role role;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JsonIgnore
@@ -32,4 +38,35 @@ public class User{
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JsonIgnore
     private List<LikedDislikePlaylist> likedDislikePlaylistList = new ArrayList<>();
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
 }
