82a5558c51a1d15a4e5cabf2fbca5e4dc04cf01d
mohamed haroun saleh <es-mosaleh.saleh2024@alexu.edu.eg>
2024-08-28 02:55:19 +0300

    Adding JWT Authentication Filter adding Sercurity Configuration will be customized soon creating SigninRequest, signUpRequest implementing UserDetails

---
+++ src/main/java/com/example/Spotify/dto/SignUpRequest.java
@@ -0,0 +1,16 @@
+package com.example.Spotify.dto;
+
+
+import com.example.Spotify.enums.Role;
+import lombok.Data;
+
+@Data
+public class SignUpRequest {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    private Role role;
+}
