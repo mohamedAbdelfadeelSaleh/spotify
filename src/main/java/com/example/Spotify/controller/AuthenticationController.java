e418a3accb7906e95472451511ebb6bf4eb58cff
mohamed haroun saleh <es-mosaleh.saleh2024@alexu.edu.eg>
2024-08-28 04:21:22 +0300

    adding authentication and authorization only customization is needed to make it fit this application

--- src/main/java/com/example/Spotify/controller/AuthenticationController.java
+++ src/main/java/com/example/Spotify/controller/AuthenticationController.java
@@ -7,7 +7,6 @@ import com.example.Spotify.dto.SigninRequest;
 import com.example.Spotify.model.User;
 import com.example.Spotify.service.AuthenticationService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
