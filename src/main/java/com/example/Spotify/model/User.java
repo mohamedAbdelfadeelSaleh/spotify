5b2da6652b1c68f266ce9e0e8285bfbc757a1b65
mhmdh <es-mosaleh.saleh2024@alexu.edu.eg>
2024-09-12 18:15:00 +0300

    Revert "removing security for now"
    This reverts commit 2c8e5fa65ac4a558f0b3d471e84df2498d1b4aa8.

--- src/main/java/com/example/Spotify/model/User.java
+++ src/main/java/com/example/Spotify/model/User.java
@@ -7,7 +7,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -16,7 +20,7 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 @Table(name = "_user")
-public class User{
+public class User implements UserDetails {
     @Id
     @GeneratedValue
     private Long id;
@@ -33,5 +37,36 @@ public class User{
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     @JsonIgnore
     private List<LikedDislikedPlaylist> likedDislikedPlaylistList = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public String getUsername() {
+        return getEmail();
+    }
+
 
 }
